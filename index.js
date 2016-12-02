6b73c2de7af09546c055d6988584ccefefc13750
keypatel_i <Keydsc@000>
2016-12-02 16:57:38 -0500

    Updated

--- index.js
+++ index.js
@@ -6,10 +6,32 @@
 var express = require('express');
 var app = express();
 
+var queue = async.queue(SendEmail, 1);
+// assign a callback
+queue.drain = function() {
+    console.log('all items have been processed');
+};
 app.get('/', function (req, res) {
     res.send('Hello World!');
 })
 
 app.listen(9003, function () {
     console.log('Node app listening on port 9003...!!!');
-})
+});
+
+function pushNotification() {
+    queue.push({url: "/users/1", data: {attrib: "value"}, method: "POST"}, function (error) {
+        if (error) {
+            lconsole.log(error);
+        }
+        else {
+            console.log("Finished processing: ", url);
+        }
+    });
+
+}
+
+function sendNotification(data, next) {
+    console.log("Received Data: ", data);
+    next(null, true);
+}
